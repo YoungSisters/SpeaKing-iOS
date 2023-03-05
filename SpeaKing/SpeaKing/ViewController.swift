8443c962e57e852201c9f11be54a04cea53a04c6
Seoyoung Lee <wow9176@naver.com>
2023-03-05 16:50:16 +0900

    init: 프로젝트 생성

---
+++ SpeaKing/SpeaKing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeaKing
+//
+//  Created by 이서영 on 2023/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpeaKing/SpeaKingUITests/SpeaKingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpeaKingUITestsLaunchTests.swift
+//  SpeaKingUITests
+//
+//  Created by 이서영 on 2023/03/05.
+//
+
+import XCTest
+
+final class SpeaKingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
